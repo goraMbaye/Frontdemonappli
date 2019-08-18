ec973c6aa635155e98b3b5a6efd57b53d99c46bd
gora mbaye <g.mbaye3182@zig.univ.sn>
2019-08-18 03:22:15 +0100

    monPremierApplicationAngular

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import{Routes ,RouterModule, Router, RoutesRecognized}from'@angular/router';
+import{GstAddComponent}from './gst-add/gst-add.component';
+import { GstEditComponent } from './gst-edit/gst-edit.component';
+import { GstGetComponent } from './gst-get/gst-get.component';
+const routes: Routes = [
+  {
+    path: 'business/create',
+    component: GstAddComponent
+  },
+  {
+    path: 'business/edit/:id',
+    component: GstEditComponent
+  },
+  {
+    path: 'business',
+    component: GstGetComponent
+  }
+];
+
+
+@NgModule({
+  declarations: [],
+  imports: [ RouterModule.forRoot(routes)],
+  exports:[RouterModule]
+  
+})
+export class AppRoutingModule { }
